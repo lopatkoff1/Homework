ff453794b2a83645bb99d49996a8121d631077ee
Pavel <kgk174@bk.ru>
2023-01-19 15:59:38 +0300

    Task-01

---
+++ Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+
+if (num_a > num_b)
+{
+    Console.WriteLine("Первое число " + num_a + " больше чем второе " + num_b);
+}
+else
+{
+    Console.WriteLine("Второе число " + num_b + " больше чем первое " + num_a);
+}
