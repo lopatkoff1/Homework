2ed99e573447ea56db92044a0ca0f4849749530f
Pavel <kgk174@bk.ru>
2023-01-19 17:12:50 +0300

    task-04

---
+++ Task_04/Program.cs
@@ -0,0 +1,18 @@
+﻿int i = 1;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + "");
+        
+    }
+    i++;
+}
+
+
+   
+
