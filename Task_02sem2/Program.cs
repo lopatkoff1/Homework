4cc7bafab3c720e556fa1bf1c2213b1a0ac0ac7e
Pavel <kgk174@bk.ru>
2023-01-24 17:28:53 +0300

    Task 2sem2

---
+++ Task_02sem2/Program.cs
@@ -0,0 +1,26 @@
+﻿ //Задача 2: Напишите программу, которая выводит третью цифру
+ // заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//326792 -> 6
+
+int IntputInt(string msg)
+{
+    Console.WriteLine(msg + " > ");
+    string inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+int numIn = IntputInt("Введите число");
+if(numIn / 100 == 0)
+{
+    Console.WriteLine("Третей цифры нет");
+}
+else
+{
+    while(numIn > 1000)
+    {
+        numIn = numIn / 10;
+    }
+    Console.WriteLine($"Третья цифра:{numIn % 10}");
+}
