29c4373f4abe6d5388026f0a855b11b87f9ccf75
Pavel <kgk174@bk.ru>
2023-01-30 16:23:02 +0300

    Sem_4 Task03

---
+++ Seminar4_Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая задаёт массив из 8
+ //элементов и выводит их на экран.
+//1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+// -> 8
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] Array = new int[lenArray];
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(1,9);
+    Console.Write(Array[i] + " ");
+}
+
+
+
