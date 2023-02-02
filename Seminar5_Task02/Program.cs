d2df8f599b423c28fa821f9da63ded464776da67
Pavel <kgk174@bk.ru>
2023-02-02 16:01:50 +0300

    Task02_Sem5

---
+++ Seminar5_Task02/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами.
+//   Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+int size = 6;
+int[] numbers = new int[size];
+FillArray(numbers);
+PrintArray(numbers);
+
+int sumNumbers = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sumNumbers += numbers[i];
+}
+Console.Write(sumNumbers);
+
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
