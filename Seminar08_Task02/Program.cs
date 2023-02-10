05c847ffca2facc4dd3eca7f5879311da8bcc63e
Pavel <kgk174@bk.ru>
2023-02-10 11:00:07 +0300

    Sem08_Task02

---
+++ Seminar08_Task02/Program.cs
@@ -0,0 +1,56 @@
+﻿//  Задайте прямоугольный двумерный массив. Напишите программу,
+//   которая будет находить строку с наименьшей суммой элементов.
+
+
+int[,] matrix = new int[4, 4];
+
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+FillArrayRandom(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+NumberRowMinSumElements(matrix);
+
+
